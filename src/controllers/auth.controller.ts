cff89be95d93b577e6451a47cb8196a6e85aa5fa
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-12-01 01:11:39 +0530

    Created refreshHandler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,11 @@
 import errorBoundary from "../utils/errorBoundary"
-import { createAccount, loginUser } from "../services/auth.service"
-import { CREATED, OK } from "../constants/http"
-import { clearAuthCookies, setAuthCookies } from "../utils/cookies"
+import { createAccount, loginUser, refreshUserAccessToken } from "../services/auth.service"
+import { CREATED, OK, UNAUTHORIZED } from "../constants/http"
+import { clearAuthCookies, getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthCookies } from "../utils/cookies"
 import { loginSchema, registerSchema } from "./auth.schemas"
 import { verifyToken } from "../utils/jwt"
 import SessionModel from "../models/session.model"
+import appAssert from "../utils/appAssert"
 
 export const registerHandler = errorBoundary(async (req, res) => {
   // validate request
@@ -47,4 +48,22 @@ export const logoutHandler = errorBoundary(async (req, res) => {
   return clearAuthCookies(res).status(OK).json({
     message: "Logout successful"
   })
+})
+
+export const refreshHandler = errorBoundary(async (req, res) => {
+  const refreshToken = req.cookies["refreshToken"]
+  appAssert(refreshToken, UNAUTHORIZED, "Missing refresh token")
+
+  const { accessToken, newRefreshToken } = await refreshUserAccessToken(refreshToken)
+
+  if (newRefreshToken) {
+    res.cookie("refreshToken", newRefreshToken, getRefreshTokenCookieOptions())
+  }
+
+  return res
+    .status(OK)
+    .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+    .json({
+      message: "Access token refreshed"
+    })
 })
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,8 +4,8 @@ import SessionModel from "../models/session.model"
 import UserModel from "../models/user.model"
 import VerificationCodeModel from "../models/verificationCode.model"
 import appAssert from "../utils/appAssert"
-import { oneYearFromNow } from "../utils/date"
-import { refreshTokenSignOptions, signToken } from "../utils/jwt"
+import { ONE_DAY_MS, oneYearFromNow, thirtyDaysFromNow } from "../utils/date"
+import { RefreshTokenPayload, refreshTokenSignOptions, signToken, verifyToken } from "../utils/jwt"
 
 type CreateAccountParams = {
   email: string,
@@ -24,7 +24,7 @@ export const createAccount = async (data: CreateAccountParams) => {
     email: data.email,
     password: data.password
   })
-  
+
   const userId = user._id
 
   // create verification code
@@ -91,5 +91,35 @@ export const loginUser = async ({ email, password, userAgent }: LoginUserParams)
     user: user.omitPassword(),
     accessToken,
     refreshToken
+  }
+}
+
+export const refreshUserAccessToken = async (refreshToken: string) => {
+  const { payload, error } = verifyToken<RefreshTokenPayload>(refreshToken, {
+    secret: refreshTokenSignOptions.secret
+  })
+  appAssert(payload, UNAUTHORIZED, "Invalid refresh token")
+
+  const now = Date.now()
+  const session = await SessionModel.findById(payload.sessionId)
+  appAssert(session && session.expiresAt.getTime() > now, UNAUTHORIZED, "Session expired")
+
+  // refresh session if it is expiring in the next 24 hours for better user experience
+  const sessionNeedsRefresh = session.expiresAt.getTime() - now <= ONE_DAY_MS
+  if (sessionNeedsRefresh) {
+    session.expiresAt = thirtyDaysFromNow()
+    await session.save()
+  }
+
+  const newRefreshToken = sessionNeedsRefresh ? signToken({ sessionId: session._id }, refreshTokenSignOptions) : undefined
+
+  const accessToken = signToken({
+    userId: session.userId,
+    sessionId: session._id
+  })
+
+  return {
+    accessToken,
+    newRefreshToken
   }
 }
