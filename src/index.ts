acdac1ba7345d62b3e97c9759950b74b2312e306
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-11-30 19:36:09 +0530

    Added additional express middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import connectToDatabase from './lib/db'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
-const PORT = process.env.PORT || 3000
-const NODE_ENV = process.env.NODE_ENV
+import { CLIENT_ORIGIN, NODE_ENV, PORT } from './constants/env'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: CLIENT_ORIGIN,
+  credentials: true,
+}))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -11,4 +21,7 @@ app.get('/', (req, res) => {
   })
 })
 
-app.listen(PORT, () => console.log(`[ENV=${NODE_ENV}] App listening on port ${PORT}`))
+app.listen(PORT, async () => {
+  console.log(`[NODE_ENV=${NODE_ENV}] App listening on port ${PORT}`)
+  await connectToDatabase()
+})
