02b1f493b8bc3b061185254e6184541a13d0a1a7
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-12-01 01:27:52 +0530

    Created verifyEmailHandler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,8 @@
 import errorBoundary from "../utils/errorBoundary"
-import { createAccount, loginUser, refreshUserAccessToken } from "../services/auth.service"
+import { createAccount, loginUser, refreshUserAccessToken, verifyEmail } from "../services/auth.service"
 import { CREATED, OK, UNAUTHORIZED } from "../constants/http"
 import { clearAuthCookies, getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthCookies } from "../utils/cookies"
-import { loginSchema, registerSchema } from "./auth.schemas"
+import { loginSchema, registerSchema, verificationCodeSchema } from "./auth.schemas"
 import { verifyToken } from "../utils/jwt"
 import SessionModel from "../models/session.model"
 import appAssert from "../utils/appAssert"
@@ -66,4 +66,14 @@ export const refreshHandler = errorBoundary(async (req, res) => {
     .json({
       message: "Access token refreshed"
     })
+})
+
+export const verifyEmailHandler = errorBoundary(async (req, res) => {
+  const verificationCode = verificationCodeSchema.parse(req.params.code)
+
+  await verifyEmail(verificationCode)
+
+  return res.status(OK).json({
+    message: "Email was verified successfully"
+  })
 })
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { CONFLICT, UNAUTHORIZED } from "../constants/http"
+import { CONFLICT, INTERNAL_SERVER_ERROR, NOT_FOUND, UNAUTHORIZED } from "../constants/http"
 import VerificationCodeType from "../constants/verificationCodeTypes"
 import SessionModel from "../models/session.model"
 import UserModel from "../models/user.model"
@@ -121,5 +121,27 @@ export const refreshUserAccessToken = async (refreshToken: string) => {
   return {
     accessToken,
     newRefreshToken
+  }
+}
+
+export const verifyEmail = async (code: string) => {
+  // get verification code
+  const validCode = await VerificationCodeModel.findOne({
+    _id: code,
+    type: VerificationCodeType.EmailVerification,
+    expiresAt: { $gt: new Date() }
+  })
+  appAssert(validCode, NOT_FOUND, "Invalid or expired verification code")
+
+  // get user by id and set verified = true
+  const updatedUser = await UserModel.findByIdAndUpdate(validCode.userId, { verified: true }, { new: true })
+  appAssert(updatedUser, INTERNAL_SERVER_ERROR, "Failed to verify email")
+
+  // delete verification code
+  await validCode.deleteOne();
+  
+  // return user
+  return {
+    user: updatedUser.omitPassword()
   }
 }
