b2f8524af01c09522168c64d8caad57826f9ecb8
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-12-27 21:36:51 +0530

    Created protected routes wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register"
 import VerifyEmail from "./pages/VerifyEmail"
 import ForgotPassword from "./pages/ForgotPassword"
 import ResetPassword from "./pages/ResetPassword"
+import AppContainer from "./components/AppContainer"
 
 const Home = () => {
   return (
@@ -15,7 +16,9 @@ const Home = () => {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<AppContainer />}>
+        <Route index element={<Home />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/email/verify/:code" element={<VerifyEmail />} />
---
+++ src/components/AppContainer.jsx
@@ -0,0 +1,50 @@
+import { Box, CircularProgress } from '@mui/material'
+import useAuth from '../hooks/useAuth'
+import { Navigate, Outlet } from 'react-router-dom'
+
+function AppContainer() {
+  const { user, isLoading } = useAuth()
+
+  return (
+    <>
+      {
+        isLoading && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              gap: "1rem",
+              my: "3rem"
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        )
+      }
+      {
+        !isLoading && user && (
+          <>
+            {/* <UserMenu /> */}
+            <Outlet />
+          </>
+        )
+      }
+      {
+        !isLoading && !user && (
+          <Navigate
+            to="/login"
+            replace
+            state={{
+              redirectUrl: window.location.pathname
+            }}
+          />
+        )
+      }
+    </>
+  )
+}
+
+export default AppContainer
